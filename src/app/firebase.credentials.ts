bfb3c66412f9c691db3bf5d97b69641d7335e12c
Jeanne <jeanneu@careercruising.com>
2017-08-22 10:06:08 -0400

    Firebase credentials and project setup

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN.firebaseapp.com",
+    databaseURL: "https://YOUR_DB_URL.firebaseio.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID"
+  };
