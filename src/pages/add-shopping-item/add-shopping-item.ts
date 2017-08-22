0ca373ad89911a13a535917fb8c722d3534eaa97
Jeanne <jeanneu@careercruising.com>
2017-08-22 14:53:50 -0400

    Add shopping Item model and use it to bind a new item

--- src/pages/add-shopping-item/add-shopping-item.ts
+++ src/pages/add-shopping-item/add-shopping-item.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface';
+
 @Component({
   selector: 'page-add-shopping-item',
   templateUrl: 'add-shopping-item.html',
 })
 export class AddShoppingItemPage {
 
+  shoppingItem = {} as ShoppingItem;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddShoppingItemPage');
+  addShoppingItem() {
+    console.log(this.shoppingItem);
   }
 
 }
